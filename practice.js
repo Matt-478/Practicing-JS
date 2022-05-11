e2dd57eddb26d287cea2dd2444659dc44934e669
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-11 15:26:44 +0300

    setup exercise 3

--- practice.js
+++ practice.js
@@ -11,17 +11,24 @@
 
 
 // Bitwise Operations
-function bitwiseAND(n1, n2) {
-	let result = n1 & n2
-  return result
-}
+// function bitwiseAND(n1, n2) {
+// 	let result = n1 & n2
+//   return result
+// }
 
-function bitwiseOR(n1, n2) {
-	let result = n1 | n2
-  return result
-}
+// function bitwiseOR(n1, n2) {
+// 	let result = n1 | n2
+//   return result
+// }
 
-function bitwiseXOR(n1, n2) {
-	let result = n1 ^ n2
-	return result
+// function bitwiseXOR(n1, n2) {
+// 	let result = n1 ^ n2
+// 	return result
+// }
+
+
+// Which Function Returns the Larger Number?
+// Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
+function whichIsLarger(f, g) {
+	
 }
