a9d846a89a3f5951221a310dc61f1d6f7ddf46e9
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-11 15:36:39 +0300

    ex.3 done

--- practice.js
+++ practice.js
@@ -29,6 +29,21 @@
 
 // Which Function Returns the Larger Number?
 // Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
-function whichIsLarger(f, g) {
-	
-}
+// function whichIsLarger(f, g) {
+// 	if(f > g) {
+// 		console.log("f")
+// 		return("f")
+// 	}
+// 	else if(f < g) {
+// 		console.log("g")
+// 		return("g")
+// 	} else {
+// 		console.log("neither")
+// 		return("neither")
+// 	}
+// }
+// whichIsLarger(5, 10)
+// whichIsLarger(10, 5)
+// whichIsLarger(25, 25)
+// whichIsLarger(100, 10)
+// whichIsLarger(5, 20)
