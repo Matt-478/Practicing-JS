b86ae810cf73cff94b9a6b184431b01456a10054
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-12 19:04:13 +0300

    ex 4 done

--- practice.js
+++ practice.js
@@ -47,3 +47,23 @@
 // whichIsLarger(25, 25)
 // whichIsLarger(100, 10)
 // whichIsLarger(5, 20)
+
+
+
+
+
+
+// Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+// function timeForMilkAndCookies(date) {
+//   if(date.getDate() === 24 && date.getMonth() === 11) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// timeForMilkAndCookies(new Date(2013, 11, 24))
+// timeForMilkAndCookies(new Date(3000, 11, 24))
+// timeForMilkAndCookies(new Date(2154, 11, 11))
+// timeForMilkAndCookies(new Date(2010, 11, 2))
+// timeForMilkAndCookies(new Date(1980, 9, 24))
