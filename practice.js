8d35091a90d4bbbde1d29e30f7252574df3156be
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-13 16:56:45 +0300

    Ex 12 done

--- practice.js
+++ practice.js
@@ -200,3 +200,19 @@
 // myFunction(1, '1')
 // // Expected false 
 // myFunction('10', '10')
+
+
+
+// EX 12
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+// function myFunction(a){
+//   return a.slice(0, 3)
+// }
+
+// myFunction('abcdefg')
+// // Expected 'abc'
+// myFunction('1234')
+// // Expected '123'
+// myFunction('fgedcba')
