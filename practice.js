26089028a23db070816104fdd217fe06d216a0f3
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-13 17:46:11 +0300

    Ex 14 done

--- practice.js
+++ practice.js
@@ -234,3 +234,21 @@
 // // Expected '234'
 // myFunction('fgedcba')
 // // Expected 'cba'
+
+
+
+
+
+// EX 14
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
+// function myFunction(a){
+//   return obj = { key: a }
+// }
+// myFunction('a')
+// // Expected {key:'a'}
+// myFunction('z')
+// // Expected {key:'z'}
+// myFunction('b')
+// // Expected {key:'b'}
